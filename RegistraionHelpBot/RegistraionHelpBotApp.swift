2c1dbb81952f9cb7b0625aaea1f756e5e5677082
Ainsley Pluamdore <a.plumadore@setonhill.edu>
2023-02-15 14:35:01 -0500

    Initial Commit

---
+++ RegistraionHelpBot/RegistraionHelpBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistraionHelpBotApp.swift
+//  RegistraionHelpBot
+//
+//  Created by Ainsley Plumadore on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct RegistraionHelpBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
