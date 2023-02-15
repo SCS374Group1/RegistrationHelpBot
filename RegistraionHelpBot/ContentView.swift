664b4c8162752cba16e792b1fa60bd43f7b921ff
Shane <sm.simpson@setonhill.edu>
2023-02-15 15:01:20 -0500

    test2

--- RegistraionHelpBot/ContentView.swift
+++ RegistraionHelpBot/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world abc")
+            Text("Hello, world abc xyz")
         }
         .padding()
     }
